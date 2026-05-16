243fb59cbf5048d4d3490359ffb56a8e82724a61
shamshad <shamshadalam10313@gmail.com>
2026-05-16 14:45:10 +0530

    lag rha hai ho gya

--- gallery-project/src/App.jsx
+++ gallery-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import axios from "axios";
 
@@ -6,14 +6,48 @@ function App() {
   
   const [userData, setUserData] = useState([])
 
+  const [index, setindex] = useState(1)
+
   const getData = async() => {
-    const responce = await axios.get(`https://picsum.photos/v2/list?page=2&limit=100`)
-    console.log(responce.data)
+    const responce = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=15`)
+    setUserData(responce.data)
   }
+
+  let printUserData = <h1 className='loading'>Loading...</h1>
+
+  if(userData.length > 0){
+    printUserData = userData.map(function(elem, idx) {
+        return <div key={idx}>
+        <img src={elem.download_url} alt='photo'className='image'/>
+        <h3>{elem.author}</h3>
+        </div>
+    })
+  }
+
+  useEffect(function(){
+    getData()
+  }, [index])
+
 
   return (
     <>
-      <button onClick={getData}>submit</button>
+    <div>
+      <div className='main'>
+        {printUserData}
+      </div>
+      <div className='buttonDiv'>
+        <button className='button' onClick={function() {
+          setindex(index-1)
+          setUserData([])
+        }}>Prev</button>
+        <button className='button'>{index}</button>
+        <button className='button' onClick={function(){
+          setindex(index+1)
+          setUserData([])
+        }}>Next</button>
+      </div>
+    </div>
+      
     </>
   )
 }
